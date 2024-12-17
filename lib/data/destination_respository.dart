226775500cbaadd49b770756f0d67e4818d9c147
David <davejohngoddard@gmail.com>
2024-12-17 16:38:34 +0000

    [NOM-15] - Created City_details screen, just playing around with appbar now

--- lib/data/destination_respository.dart
+++ lib/data/destination_respository.dart
@@ -9,7 +9,11 @@ List<Country> _allCountries = List.generate(100, (index) {
 });
 List<City> _allCities = List.generate(500, (index) {
   Random random = Random();
-  return City(index, 'City$index', 'some city description', random.nextInt(100 + 1));
+  return City(
+      index,
+      'City$index',
+      'Lorem ipsum odor amet, consectetuer adipiscing elit. Fringilla id nibh dapibus quam, porttitor dignissim sagittis. Inceptos maecenas odio luctus mattis efficitur quis eros est. Leo ante nullam phasellus; pulvinar eleifend integer donec ridiculus pretium. Non facilisi sem; posuere nullam nibh ligula pellentesque. Mus nisl sem ultrices; tempor pellentesque malesuada. Torquent nostra quam sollicitudin ac fermentum sociosqu. Augue habitasse fames purus tincidunt justo porttitor placerat ut. Pulvinar posuere varius sit vivamus, justo cursus. Fames libero nulla quam curabitur blandit sed.Hac at congue lacus auctor magnis. Condimentum amet iaculis potenti fusce et diam diam pharetra dapibus. Sapien consectetur tincidunt etiam vivamus metus hendrerit felis tempor. Leo efficitur a nam blandit; massa accumsan. Pretium duis curae curabitur accumsan faucibus ex et. Viverra mauris pharetra est dapibus per nostra proin. Arcu natoque maecenas etiam dui nisi dolor sed. Porttitor curabitur suspendisse orci lobortis id rhoncus litora porttitor. Ultrices sapien sociosqu diam malesuada massa non nibh quis.Risus fringilla porta taciti diam elit duis nunc. Semper euismod nascetur egestas ullamcorper; cubilia semper convallis enim. Viverra ultricies volutpat habitant elementum sapien eget finibus. Cursus magnis nibh dapibus vehicula mollis semper nullam. Suspendisse iaculis vivamus, cras duis tempus scelerisque. Maximus platea parturient taciti pellentesque massa cubilia. Neque tempus tortor imperdiet eget nisl vitae semper.',
+      random.nextInt(100 + 1));
 });
 
 class DestinationRepository {
--- lib/screens/city_details_screen.dart
+++ lib/screens/city_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../domain/city.dart';
+import '../widgets/page_title.dart';
 
 class CityDetailsScreen extends StatelessWidget {
   const CityDetailsScreen({super.key, required this.selectedCity});
@@ -10,9 +11,104 @@ class CityDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(selectedCity.getName),
+      body: SafeArea(
+        child: Column(
+          children: [
+            PageTitle(
+              titleText: selectedCity.getName,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    height: 200,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            alignment: Alignment.centerLeft,
+                            child: Icon(selectedCity.getIcon),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                CityRating(
+                                  score: 9,
+                                  ratingIcon: Icons.sailing,
+                                ),
+                                CityRating(
+                                  score: 9,
+                                  ratingIcon: Icons.restaurant,
+                                ),
+                                CityRating(
+                                  score: 9,
+                                  ratingIcon: Icons.local_bar,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(selectedCity.getDescription),
+                  )
+                ],
+              ),
+            )
+
+          ],
+        ),
       )
     );
   }
 }
+
+class CityRating extends StatelessWidget {
+  const CityRating({super.key, required this.score, required this.ratingIcon});
+
+  final int score;
+  final IconData ratingIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '$score/10',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: FittedBox(
+                alignment: Alignment.centerLeft,
+                fit: BoxFit.contain,
+                child: Icon(ratingIcon),
+              ),
+            )
+          ],
+        ),
+    );
+  }
+}
+
