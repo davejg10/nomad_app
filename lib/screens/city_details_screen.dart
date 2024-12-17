d1259a7b70638a56405162f813229f12d488dee4
David <davejohngoddard@gmail.com>
2024-12-17 17:18:16 +0000

    [NOM-15] - Added backnavigation & loop to display city criteria

--- lib/screens/city_details_screen.dart
+++ lib/screens/city_details_screen.dart
@@ -62,3 +62,42 @@ class CityDetailsScreen extends StatelessWidget {
     );
   }
 }
+
+
+class CityRating extends StatelessWidget {
+  const CityRating({super.key, required this.score, required this.ratingIcon});
+
+  final int score;
+  final IconData ratingIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '$score/10',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 10,),
+            Expanded(
+              child: FittedBox(
+                alignment: Alignment.centerLeft,
+                fit: BoxFit.contain,
+                child: Icon(ratingIcon),
+              ),
+            )
+          ],
+        ),
+    );
+  }
+}
