c2bd629588d19f6e99958d4a56e32ce2358930f0
David <davejohngoddard@gmail.com>
2024-12-18 11:08:43 +0000

    [NOM-16] - Introduced Appbar and have undo and search tools added to bar

--- lib/global_screen.dart
+++ lib/global_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class GlobalScreen extends StatelessWidget {
-  GlobalScreen({super.key, required this.child});
+  GlobalScreen({super.key, this.appBar, required this.child});
 
+  AppBar? appBar;
   Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appBar,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
