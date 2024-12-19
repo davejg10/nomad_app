e1aa3b880cbf2de503fbdd7a977b1ee7ccf77320
davejg10 <davejohngoddard@gmail.com>
2024-12-19 08:16:26 +0000

    NOM-15/city-details-screen (#4)
    This PR contains the following changes;
    
    - Added cityRatings field to each city which is a map containing each criteria the city can be rating on and the score. At the moment this is the same for all cities.
    - Added the CityDetails screen as shown by the image;
    
    ![image](https://github.com/user-attachments/assets/699642a8-3f03-479b-a09c-2c32e99885b9)

--- lib/domain/city.dart
+++ lib/domain/city.dart
@@ -6,6 +6,11 @@ class City {
   final int _id;
   final String _name;
   final String _description;
+  final Map<String, int> _cityRatings = {
+    'sailing': 9,
+    'food': 7,
+    'nightlife': 8
+  };
   final int _countryId;
 
   City(this._id, this._name, this._description, this._countryId);
@@ -14,5 +19,19 @@ class City {
   int get getId => _id;
   String get getName => _name;
   String get getDescription => _description;
+  Map<String, int> get getCityRatings => _cityRatings;
   int get getCountryId => _countryId;
+
+  static IconData convertCriteriaToIcon(String criteria) {
+    switch (criteria) {
+      case 'sailing':
+        return Icons.sailing;
+      case 'food':
+        return Icons.restaurant;
+      case 'nightlife':
+        return Icons.local_bar;
+      default:
+        throw Exception('no mapping for criterian');
+    }
+  }
 }
--- lib/screens/city_details_screen.dart
+++ lib/screens/city_details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../domain/city.dart';
+import '../widgets/city_rating.dart';
+import '../widgets/page_title.dart';
 
 class CityDetailsScreen extends StatelessWidget {
   const CityDetailsScreen({super.key, required this.selectedCity});
@@ -10,8 +12,66 @@ class CityDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(selectedCity.getName),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                PageTitle(
+                  titleText: selectedCity.getName,
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.black,
+              height: 0,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    height: 200,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            alignment: Alignment.centerLeft,
+                            child: Icon(selectedCity.getIcon),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: selectedCity.getCityRatings.entries.map((entry) {
+                                return CityRating(score: entry.value, ratingIcon: City.convertCriteriaToIcon(entry.key));
+                              }).toList(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(selectedCity.getDescription),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       )
     );
   }
